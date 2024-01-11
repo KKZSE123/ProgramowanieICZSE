c619e011023c247f1d280fce24025e39d00c24a3
Test <test@test.pl>
2024-01-11 07:39:26 +0100

    3w

---
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+//Napisz program który wczyta np. 5 liczb i wyswietli je w odwrotnej kolejnoœci.
+void task1()
+{
+	const unsigned short ARRAY_SIZE = 5;
+	int numbers[ARRAY_SIZE];
+
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		std::cout << "Podaj dana: ";
+		std::cin >> numbers[i];
+	}
+
+	for (int i = ARRAY_SIZE - 1; i >= 0; i--)
+	{
+		std::cout << numbers[i] << " , ";
+	}
+	std::cout << "\n";
+}
+
+//Napisz program obliczaj¹cy œredni¹ arytmetyczn¹ elemêtów w tablicy liczb ca³kowitych
+void task2()
+{
+	const unsigned short ARRAY_SIZE = 5;
+	int numbers[ARRAY_SIZE];
+
+	const unsigned short LOWER_RANGE = 5;
+	const unsigned short UPPER_RANGE = 7;
+
+
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+
+	task1();
+}
---
+++ Programowanie/LooWhileConsoleApplication1/LooWhileConsoleApplication1/LooWhileConsoleApplication1.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+using namespace std;
+
+//Napisz program, który policzy sumê cyfr podanej przez u¿ytkownika liczby.
+void task1()
+{
+	int number;
+	std::cout << "Podaj liczbê\n";
+	std::cin >> number;
+
+	int sum = 0;
+	int rest;
+
+	/*
+	if (number != 0)
+	{
+		rest = number % 10;
+		sum = sum + rest;
+		number = number / 10;
+		if (number != 0)
+		{
+			rest = number % 10;
+			sum = sum + rest;
+			number = number / 10;
+			if (number != 0)
+			{
+				rest = number % 10;
+				sum = sum + rest;
+				number = number / 10;
+				//if (number != 0) ...
+			}
+		}
+	}
+	*/
+
+	while (number != 0)
+	{
+		rest = number % 10;
+		sum = sum + rest;
+		number = number / 10;
+	}
+
+	std::cout << "Suma " << sum << "\n";
+	//4125
+}
+
+//Napisz program, który policzy NWD dwóch liczb.
+void task2()
+{
+	int firstNumber, secondNumber;
+	std::cout << "Podaj pierwsz¹ liczbê\n";
+	std::cin >> firstNumber;
+	std::cout << "Podaj drug¹ liczbê\n";
+	std::cin >> secondNumber;
+
+	int nwd;
+	/*
+	if (firstNumber < secondNumber)
+		nwd = firstNumber;
+	else
+		nwd = secondNumber;
+	*/
+	nwd = (firstNumber < secondNumber) ? firstNumber : secondNumber;
+
+	/*
+	if (secondNumber % nwd != 0
+		|| firstNumber % nwd != 0)
+	{
+		nwd--;
+		if (secondNumber % nwd != 0
+			|| firstNumber % nwd != 0)
+		{
+			nwd--;
+			if (secondNumber % nwd != 0
+				|| firstNumber % nwd != 0)
+			{
+				nwd--;
+				//if...
+			}
+		}
+	}
+	*/
+
+	//wersja 1
+	while (secondNumber % nwd != 0
+		|| firstNumber % nwd != 0)
+	{
+		--nwd;
+	}
+
+	std::cout << "NWD = " << nwd << "\n";
+
+	//wersja 2
+	nwd = 1;
+	int dividend = 2;
+	int tmpFirstNumber = firstNumber, tmpSecondNumber = secondNumber;
+	while (tmpFirstNumber >= dividend
+		&& tmpSecondNumber >= dividend)
+	{
+		if (tmpFirstNumber % dividend == 0
+			&& tmpSecondNumber % dividend == 0)
+		{
+			tmpFirstNumber = tmpFirstNumber / dividend;
+			tmpSecondNumber /= dividend;
+			nwd *= dividend;
+		}
+		else
+			dividend++;
+	}
+	std::cout << "NWD = " << nwd << "\n";
+
+	//wersja 3
+	//NWD(a, b) = a				jeœli b = 0
+	//NWD(a, b) = NWD(b, a % b) jeœli b != 0
+	int a = firstNumber, b = secondNumber;
+	while (b != 0)
+	{
+		int tmpA = a;
+		a = b;
+		b = tmpA % b;
+	}
+	nwd = a;
+	std::cout << "NWD = " << nwd << "\n";
+}
+
+//Sprawdzanie czy liczba jest palindromem.
+void task3()
+{
+	int number;
+	std::cout << "Podaj liczbê\n";
+	std::cin >> number;
+
+	//wersja 1
+
+	//obliczam iloœæ cyfr
+	int tmpNumber = number;
+	int numberOfDigit = 1;
+	while (tmpNumber >= 10)
+	{
+		numberOfDigit++;
+		tmpNumber = tmpNumber / 10;
+	}
+
+	//liczê 10 do potêgi numberOfDigit - 1
+	int leftDivided = 1;
+	while (numberOfDigit != 1)
+	{
+		leftDivided *= 10;
+		numberOfDigit--;
+	}
+
+	int rightDivided = 10;
+	int leftNumber = number;
+	int rightNumber = number;
+
+	while (leftNumber > 10)
+	{
+
+	}
+
+}
+
+int main()
+{
+	task2();
+}
